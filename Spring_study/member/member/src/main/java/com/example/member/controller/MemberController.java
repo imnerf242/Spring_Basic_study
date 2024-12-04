dfc2dd3bd92941ce1a2749a94f70f201196a3b4f
SY <sangyoon0005301@gmail.com>
2024-12-04 10:20:36 +0900

    2024/12/04/10:01

---
+++ Spring_study/member/member/src/main/java/com/example/member/controller/MemberController.java
@@ -0,0 +1,111 @@
+package com.example.member.controller;
+
+import com.example.member.dto.MemberDTO;
+import com.example.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    // 생성자 주입
+    private final MemberService memberService;
+
+    // 회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "member/save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+        return "member/login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "member/login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if (loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail", loginResult.getMemberEmail());
+            return "member/main";
+        } else {
+            // login 실패
+            return "member/login";
+        }
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 model사용
+        model.addAttribute("memberList", memberDTOList);
+        return "member/list";
+    }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable("id") Long id, Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member", memberDTO);
+        return "member/detail";
+    }
+
+    @GetMapping("/member/update")
+    public String updateForm(HttpSession session, Model model) {
+        String myEmail = (String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.updateForm(myEmail);
+        model.addAttribute("updateMember", memberDTO);
+        return "member/update";
+    }
+
+    @PostMapping("/member/update")
+    public String update(@ModelAttribute MemberDTO memberDTO) {
+        memberService.update(memberDTO);
+        return "redirect:/member/" + memberDTO.getId();
+    }
+
+    @GetMapping("/member/delete/{id}")
+    public String deleteById(@PathVariable("id") Long id){
+        memberService.deleteById(id);
+
+        return "redirect:/member/";
+    }
+
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "member/index";
+    }
+
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail){
+        System.out.println("memberEmail = " + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
+//        if(checkResult != null){
+//            return "ok";
+//        }
+//        else{
+//            return "no";
+//        }
+
+    }
+
+    @GetMapping("/board/index")
+    public String Form() {
+        return "board/index";
+    }
+}
